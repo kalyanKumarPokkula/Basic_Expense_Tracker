815736e66cb8a9af8024b690161bfcd1961a76e1
kalyan <kalyanKumarPokkula@gmail.com>
2023-06-28 06:32:32 +0530

    Added form logic in react expense tracker

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,19 @@
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+const NewExpense = (props) => {
+  const ExpenseDateHandler = (enteredExpenseData) => {
+    let expenseDate = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseDate);
+  };
+  return (
+    <div className="new_expense">
+      <ExpenseForm onSaveExpenseData={ExpenseDateHandler} />
+    </div>
+  );
+};
+
+export default NewExpense;
