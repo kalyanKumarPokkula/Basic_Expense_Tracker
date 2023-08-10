3cedf80c77f800efe52c3acc4dcceb409d3ef6db
kalyan <kalyanKumarPokkula@gmail.com>
2023-08-10 12:57:57 +0530

    added some change to expense tracker basic

---
+++ src/components/Expense/Expense.js
@@ -0,0 +1,61 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expense.css";
+import ExpenseFilter from "./ExpenseFilter";
+import ExpenseChart from "./ExpenseChart";
+import { useState } from "react";
+
+function Expense(props) {
+  const [expenseFilter, setExpenseFilter] = useState("2021");
+
+  const expenseFilterHandler = entertedExpenseFilterYear => {
+    setExpenseFilter(entertedExpenseFilterYear);
+    console.log(entertedExpenseFilterYear);
+  };
+
+  const filteredExenses = props.expenses.filter(expense => {
+    return expense.date.getFullYear().toString() === expenseFilter;
+  });
+
+  return (
+    <div className="expense">
+      <ExpenseFilter
+        selected={expenseFilter}
+        onExpenseFilterData={expenseFilterHandler}
+      />
+
+      {filteredExenses.length === 0 && <h2>No expenses are found.</h2>}
+      <ExpenseChart expenses={filteredExenses} />
+      {filteredExenses.length > 0 &&
+        filteredExenses.map(expense => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      {/* <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      />
+      <ExpenseItem
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      />
+      <ExpenseItem
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+      />
+      <ExpenseItem
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}
+      /> */}
+    </div>
+  );
+}
+
+export default Expense;
---
+++ src/components/Expense/ExpenseFilter.js
@@ -0,0 +1,23 @@
+import "./ExpenseFilter.css";
+
+function ExpenseFilter(props) {
+  const expenseFilterHandler = (event) => {
+    props.onExpenseFilterData(event.target.value);
+  };
+
+  return (
+    <div className="expense-filter">
+      <div className="expense-filter__control">
+        <label>Filter by Year</label>
+        <select value={props.selected} onChange={expenseFilterHandler}>
+          <option value="2023">2023</option>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseFilter;
